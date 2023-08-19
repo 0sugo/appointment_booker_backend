fa06de8500b57d4643be30e2ecbc77d329a19aef
oguntayomicheal <oguntayomicheal@gmail.com>
2023-08-19 13:02:51 +0100

    Add v1 folder to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    devise_for :users, controllers: {
-      registrations: 'api/registrations'
-    }
+    namespace :v1 do
+      devise_for :users, controllers: {
+        registrations: 'api/v1/registrations'
+      }
+    end
   end
 end
