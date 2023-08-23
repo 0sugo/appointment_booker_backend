f3619b183839f0dc88812445998369d283d60276
0sugo <josugo26@gmail.com>
2023-08-23 13:08:12 +0300

    Customise resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         registrations: 'api/v1/registrations',
         sessions: 'api/v1/sessions'
       }
-      resources :reservations
+      resources :reservations only: %i[index create destroy]
     end
   end
 
