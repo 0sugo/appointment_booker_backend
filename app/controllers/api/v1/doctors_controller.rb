b1bf48ad33a29e2bcb19cc348061b77a3ec05932
Abdurahim .M <Abdugreat2@gmail.com>
2023-08-24 19:45:49 +0300

    Add show doctor action and route

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::DoctorsController < ApplicationController
     @doctors = Doctor.all
     render json: @doctors
   end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+    render json: { status: 'Success', message: 'Entry retrieved succesfully', data: @doctor }, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { status: 'Not Found', message: 'Something went wrong' }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         registrations: 'api/v1/registrations',
         sessions: 'api/v1/sessions'
       }
-      resources :doctors
+      resources :doctors, only: [:index, :show, :new, :create]
       resources :reservations, only: [:index, :create, :destroy]
     end
   end
