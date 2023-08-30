2f9aeac93f315c92e6eb358225012814581b4314
UbdaNam <abdugreat2@gmail.com>
2023-08-30 04:21:24 -0700

    Add duration column to doctors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_225858) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_111522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_225858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.decimal "price", precision: 10, scale: 2
+    t.string "duration"
   end
 
   create_table "reservations", force: :cascade do |t|
