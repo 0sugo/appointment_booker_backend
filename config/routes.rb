811ddef1f5fdbb365a4b81a457d4586833a276a7
UbdaNam <abdugreat2@gmail.com>
2023-08-30 04:20:36 -0700

    Remove unwanted route for doctor

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         registrations: 'api/v1/registrations',
         sessions: 'api/v1/sessions'
       }
-      resources :doctors, only: [:index, :show, :new, :create]
+      resources :doctors, only: [:index, :show, :create]
       resources :reservations, only: [:index, :create, :destroy]
     end
   end
