a843298805d9b056c1bcd4457c1c938f73e30f92
0sugo <josugo26@gmail.com>
2023-08-31 21:17:46 +0300

    Add dependent destroy on reservations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
   validates :name, :specialisation, :city, :price, :duration, presence: true
 end
